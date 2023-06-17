f1b14bf1837f62f061d1f67a201b461d7806bfef
San <santhoshvijayakumar847@gmail.com>
2023-06-17 12:40:44 +0530

    container body

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,25 +5,20 @@ window.addEventListener("load", function () {
 	// }, 2000);
 });
 
-function isElementVisible(element) {
-	var rect = element.getBoundingClientRect();
-	var viewportHeight =
-		window.innerHeight || document.documentElement.clientHeight;
-	var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-
-	return (
-		rect.top <= viewportHeight &&
-		rect.bottom >= 0 &&
-		rect.left <= viewportWidth &&
-		rect.right >= 0
+function checkVisible(elm) {
+	var rect = elm.getBoundingClientRect();
+	var viewHeight = Math.max(
+		document.documentElement.clientHeight,
+		window.innerHeight
 	);
+	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
 }
 
 function reveal() {
 	var reveals = document.querySelectorAll(".reveal");
 
 	for (var i = 0; i < reveals.length; i++) {
-		if (isElementVisible(reveals[i])) {
+		if (checkVisible(reveals[i])) {
 			reveals[i].classList.add("active");
 		} else {
 			reveals[i].classList.remove("active");
