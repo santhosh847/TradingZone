d29c3311c9e24845fcbd92f03c8c835bd4b4b83d
San <santhoshvijayakumar847@gmail.com>
2023-06-17 11:34:35 +0530

    added animations

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,29 @@
 let loader = document.getElementById("preloader");
 window.addEventListener("load", function () {
-	setTimeout(() => {
-		loader.style.display = "none";
-	}, 2000);
+	// setTimeout(() => {
+	loader.style.display = "none";
+	// }, 2000);
 });
+
+function checkVisible(elm) {
+	var rect = elm.getBoundingClientRect();
+	var viewHeight = Math.max(
+		document.documentElement.clientHeight,
+		window.innerHeight
+	);
+	return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+}
+
+function reveal() {
+	var reveals = document.querySelectorAll(".reveal");
+
+	for (var i = 0; i < reveals.length; i++) {
+		if (checkVisible(reveals[i])) {
+			reveals[i].classList.add("active");
+		} else {
+			reveals[i].classList.remove("active");
+		}
+	}
+}
+
+window.addEventListener("scroll", reveal);
